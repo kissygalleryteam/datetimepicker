2c8d525bc6c5078ee8839dccbd84cc572c5c303f
承风 <libiao.lb@taobao.com>
2014-04-18 12:17:32 +0800

    删除了一个todo

--- 1.1/index.js
+++ 1.1/index.js
@@ -3,7 +3,6 @@
  * @author libiao.lb@tmall.com
  * @todo 持续时间标记
  * @todo stat 和 end 联动
- * @todo 样式文件引入问题
  */
 KISSY.add('gallery/datetimepicker/1.1/index', function(S, DOM, Event, Moment) {
     /**
