fd25e1efc789cde62903efd38a3b35b3eb6e9714
弘树 <tiehang.lth@alibaba-inc.com>
2014-09-23 15:16:56 +0800

    sync daily version 2.0.0

--- 2.0.0/build/mods-min.js
+++ 2.0.0/build/mods-min.js
@@ -1,2 +1,2 @@
-/*!build time : 2014-09-23 3:11:31 PM*/
+/*!build time : 2014-09-23 3:16:38 PM*/
 KISSY.config("modules",{"kg/datetimepicker/2.0.0/index":{requires:["dom","event","kg/moment/2.0.1/index","kg/datetimepicker/2.0.0/base.css"]}});
