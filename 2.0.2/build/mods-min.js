6b02585f71bca951e7627f2e71e0351002187dd7
kissyteam <tiehang.lth@taobao.com>
2015-03-09 17:04:27 +0800

    sync daily version 2.0.2

---
+++ 2.0.2/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2015-03-09 5:03:32 PM*/
+KISSY.config("modules",{"kg/datetimepicker/2.0.2/index":{requires:["dom","event","kg/moment/2.0.1/index","kg/datetimepicker/2.0.2/base.css"]}});
